7227bec0108dffdaf6fc505ee60f051e6f31cde9
anudesi <anupama.desikan@gmail.com>
2023-03-03 14:32:27 +0100

    first change to host in git hub

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+     <form>
+     <input type="text" className="search" name="search" placeholder="  Search.."/><FontAwesomeIcon icon={faSearch} />
+     
+     </form>
     </div>
   );
 }
